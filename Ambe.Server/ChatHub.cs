3bbdfae8eaa362fec531becca636fda7e180ad0f
Dzidziguri Roman <romanbachu@gmail.com>
2026-03-02 17:38:33 +0300

    Добавьте файлы проекта.

---
+++ Ambe.Server/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Ambe.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            await Clients.Others.SendAsync("UserJoined", Context.ConnectionId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+            await Clients.Others.SendAsync("UserLeft", Context.ConnectionId);
+        }
+        // Привет
+    }
+}
---
+++ Ambe.Server/Program.cs
@@ -0,0 +1,26 @@
+using Ambe.Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR(); // Добавляем сервисы SignalR
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<ChatHub>("/chathub"); // Настраиваем маршрут для хаба
+
+app.Run();
